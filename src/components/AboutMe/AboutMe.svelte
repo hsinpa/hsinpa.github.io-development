9d56e08ca3bdda22488705fbff75be4cc1cbe4c5
hsinpa <hsinpa@gmail.com>
2020-02-07 15:01:38 +0800

    Add company logo sprite

--- src/components/AboutMe/AboutMe.svelte
+++ src/components/AboutMe/AboutMe.svelte
@@ -21,4 +21,26 @@
         </div>
         
     </div>
+
+    <div class="company_banner">
+        <div class="company_logo"><img src="./sprites/logo/htc_logo.png"> <h2>HTC</h2></div>
+        <br>
+        
+        <div class="company_content">
+            <h2> <a href="https://store.steampowered.com/app/606180/Viveport_Video/">Viveport Video</a></h2>
+
+            <div class="level">
+
+                <div class="level-left">
+                    <ul>
+                        <li>Support ambisonic audio at Runtime</li>
+                        <li>Play around with ffmepg and codec</li>
+                        <li>Optimizing video load time</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
 </div>
